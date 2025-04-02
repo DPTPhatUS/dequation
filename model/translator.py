a2d8eb22e252603f6ff4259aa56fdfa6248ccfa5
DPTPhatUS <23125065@student.hcmus.edu.vn>
2025-04-02 22:31:47 +0700

    Create translator.py

---
+++ model/translator.py
@@ -0,0 +1,12 @@
+from torch import nn
+from transformers import T5ForConditionalGeneration
+
+class TeX2Eng(nn.Module):
+    def __init__(self, model_name: str, tokenizer):
+        super(TeX2Eng, self).__init__()
+        self.model = T5ForConditionalGeneration.from_pretrained(model_name)
+        self.model.resize_token_embeddings(len(tokenizer))
+    
+    def forward(self, input_ids, attention_mask, labels):
+        outputs = self.model(input_ids=input_ids, attention_mask=attention_mask, labels=labels)
+        return outputs.loss, outputs.logits
