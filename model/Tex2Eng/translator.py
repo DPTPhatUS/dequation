0817526f73028f97b6903cade5dbb733b5dcee7c
DPTPhatUS <tuanphat51sp@gmail.com>
2025-05-15 22:50:10 +0700

    speech rule engine

--- model/Tex2Eng/translator.py
+++ model/Tex2Eng/translator.py
@@ -1,5 +1,14 @@
+import subprocess
+
 from torch import nn
 from transformers import T5ForConditionalGeneration
+
+class Translator:
+    def __init__(self):
+        pass
+    
+    def tex_to_eng(self, latex: str) -> str:
+        pass
 
 class Tex2Eng(nn.Module):
     def __init__(self, model_name: str, tokenizer):
@@ -23,3 +32,30 @@ class Tex2Eng(nn.Module):
             attention_mask=attention_mask,
             **kwargs
         )
+        
+class SRE(Translator):
+    def __init__(self):
+        super().__init__()
+        
+    def tex_to_eng(self, latex):
+        process = subprocess.Popen(
+            ["./node_modules/.bin/latex-to-speech", "-d", "clearspeak", "-b", "speech", latex],
+            stdout=subprocess.PIPE,
+            stderr=subprocess.PIPE,
+            text=True
+        )
+        stdout, stderr = process.communicate()
+        if process.returncode != 0:
+            raise RuntimeError(f"SRE error: {stderr.strip()}")
+        
+        return stdout.strip()
+
+if __name__ == "__main__":
+    latex = r"x = \frac{-b \pm \sqrt{b^2 - 4ac}}{2a}"
+    sre = SRE()
+    try:
+        english = sre.tex_to_eng(latex)
+        print("LaTeX:", latex)
+        print("English:", english)
+    except Exception as e:
+        print("Error:", e)
